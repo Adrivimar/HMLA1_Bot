018e4a0848eed6b08b1a2c133f57ba3bb65bbcf0
Horacio <87150874+horacio507@users.noreply.github.com>
2021-09-18 12:08:13 +0200

    Update bot.php

--- bot.php
+++ bot.php
@@ -24,10 +24,35 @@ switch($message) {
 Ver código: https://github.com/horacio507/HMLA_Bot';
         sendMessage($chatId, $response);
         break;
+
+    case '/noticias':
+        getNoticias($chatId);
+        break;
 }
 
 function sendMessage($chatId, $response) {
     $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
     file_get_contents($url);
+}
+
+function getNoticias($chatId){
+    
+    include("simple_html_dom.php");
+
+    $context = stream_context_create(array('http:' => array('header' => 'Accept: application/xml')));
+    $url = "https://eljuande.com/feed";
+
+    $xmlstring = file_get_contents($url, false, $context);
+
+    $xml = simplexml_load_string($xmlstring, "SimpleXMLElement", LIBXML_NOCDATA);
+    $json = json_encode($xml);
+    $array = json_decode($json, TRUE);
+
+    for ($i=0; $i < 9; $i++) {
+        $titulos = $titulos."\n\n".$array['channel']['item'][$i]['title']."<a href='".$array['channel']['item'][$i]['link']."'> 
+            +info</a>";
+    }
+
+    sendMessage($chatId, $titulos);
 }
 ?>
