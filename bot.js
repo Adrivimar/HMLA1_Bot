5ef6aec8120ed3ce709415efd1b503b62549dae6
Horacio <87150874+horacio507@users.noreply.github.com>
2021-10-02 20:14:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,17 @@
 require('dotenv').config()
 const { Telegraf } = require('telegraf')
 const axios = require('axios');
+const express = require('express');
+const app = express();
+
+app.get('/bot3eso', (req, res) => {
+    res.send('eso3bot');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Servidor escuchando en el puerto 3000');
+});
+
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
@@ -20,7 +31,11 @@ bot.hears('Avisos', ctx => {
     sendAvisosMessage(ctx);
 })
 
-bot.command('/avisos', ctx => {
+bot.command('avisos', ctx => {
+    sendAvisosMessage(ctx);
+})
+
+bot.command('avisos@HMLA1_BOT', ctx => {
     sendAvisosMessage(ctx);
 })
 
@@ -31,6 +46,10 @@ bot.hears('Deberes', ctx => {
 })
 
 bot.command('deberes', ctx => {
+    sendDeberesMessage(ctx);
+})
+
+bot.command('deberes@HMLA1_BOT', ctx => {
     sendDeberesMessage(ctx);
 })
 
@@ -66,6 +85,10 @@ function sendStartMessage (ctx) {
 
                 [
                     {text: "Menú", callback_data: 'menu'}
+                ],
+
+                [
+                    {text: "Sugerencias", callback_data: 'sugerencias'}
                 ],
 
                 [
@@ -138,20 +161,11 @@ function sendDeberesMessage (ctx) {
 
 //Lista de deberes
 
-bot.hears("e", ctx =>{
-    bot.telegram.sendMessage(ctx.chat.id, "<strong>bold</strong>", {
-        reply_markup: {
-            remove_keyboard: true,
-        }
-    })
-})
-
 bot.action('continuar_deberes', ctx => {
     ctx.answerCbQuery(); //para quitar el icono de cargando del botón
-            bot.telegram.sendMessage(ctx.chat.id, "DEBERES SEMANA 27-01" + '\n\n' + 'Sin deberes que mostrar', {
+            bot.telegram.sendMessage(ctx.chat.id, "¡Hola @" + ctx.from.username + " en este excel encontras información al respecto" + "\n\n" + "https://docs.google.com/spreadsheets/d/1t4jrx9P34vjBP8sRc69-WFRF8iqJpZYJ12wjYPLYTOA/edit?usp=sharing");
 
             })
-        })
 
 //Avisos
 
@@ -204,7 +218,7 @@ bot.action('Continuar_avisos', ctx => {
 //Resumen de los avisos
 
 bot.hears("Resumen de los avisos", ctx =>{
-    bot.telegram.sendMessage(ctx.chat.id, "Sin avisos que mostrar", {
+    bot.telegram.sendMessage(ctx.chat.id, {
         reply_markup: {
             remove_keyboard: true,
         }
@@ -214,7 +228,7 @@ bot.hears("Resumen de los avisos", ctx =>{
 //Faltas de los profesores
 
 bot.hears("Faltas de los profesores", ctx =>{
-    bot.telegram.sendMessage(ctx.chat.id, "Sin faltas que mostrar", {
+    bot.telegram.sendMessage(ctx.chat.id, {
         reply_markup: {
             remove_keyboard: true,
         }
@@ -224,7 +238,7 @@ bot.hears("Faltas de los profesores", ctx =>{
 //Excursiones
 
 bot.hears("Excursiones", ctx =>{
-    bot.telegram.sendMessage(ctx.chat.id, "Sin excursiones que mostrar", {
+    bot.telegram.sendMessage(ctx.chat.id, {
         reply_markup: {
             remove_keyboard: true,
         }
@@ -234,11 +248,70 @@ bot.hears("Excursiones", ctx =>{
 //Otros
 
 bot.hears("Otros", ctx =>{
-    bot.telegram.sendMessage(ctx.chat.id, "Sin otros avisos que mostrar", {
+    bot.telegram.sendMessage(ctx.chat.id, {
         reply_markup: {
             remove_keyboard: true,
         }
     })
+})
+
+//Sugerencias
+
+bot.command('sugerencias', ctx =>  {
+
+    const menuMessage = "Selecciona una opción " + '@' + ctx.from.username
+    bot.telegram.sendMessage(ctx.chat.id, menuMessage, {
+        reply_markup: {
+            keyboard: [
+                [
+                    { text: "Sobre la clase" },
+                    { text: "Sobre el bot" }
+                ],
+
+                [
+                    { text: "❌"}
+                ]
+    
+            ],
+            resize_keyboard: true,
+            one_time_keyboard: true
+        }
+    })    
+})
+
+bot.action('sugerencias', ctx => {
+    ctx.answerCbQuery(); //para quitar el icono de cargando del botón
+
+    const menuMessage = "Selecciona una opción " + '@' + ctx.from.username
+    bot.telegram.sendMessage(ctx.chat.id, menuMessage, {
+        reply_markup: {
+            keyboard: [
+                [
+                    { text: "Sobre la clase" },
+                    { text: "Sobre el bot" }
+                ],
+
+                [
+                    { text: "❌"}
+                ]
+    
+            ],
+            resize_keyboard: true,
+            one_time_keyboard: true
+        }
+    })
+})
+
+//Sobre la clase
+
+bot.hears("Sobre la clase", ctx => {
+    bot.telegram.sendMessage(ctx.chat.id, "Si tienes alguna sugerencia, te gustaría cambiar algo... Respecto a nuestra clase. Puedes decirlo aquí." + "\n" + "🔽🔽🔽"
+    + "\n" + "https://forms.gle/CpfsoKEgzNFjNm8L9" + "\n\n" + "¡Gracias por tus sugerencias!")
+})
+
+bot.hears("Sobre el bot", ctx => {
+    bot.telegram.sendMessage(ctx.chat.id, "Si tienes alguna sugerencia, te gustaría cambiar algo... Respecto a este bot @HMLA1_BOT. Puedes decirlo aquí." + "\n" + "🔽🔽🔽"
+    + "\n" + "https://forms.gle/w2AT5Bgoej1SNevC9" + "\n\n" + "¡Gracias por tus sugerencias!")
 })
 
 //Acerca de mi
@@ -246,21 +319,27 @@ bot.hears("Otros", ctx =>{
 bot.action('acerca', ctx => {
     ctx.answerCbQuery(); //para quitar el icono de cargando del botón
     ctx.reply('El proposito de este bot es poder ayudarte a organizarte y actualizarte con avisos y novedades.' + '\n\n' + 'Información detallada del bot'
-    + '\n\n' + 'Creador: @horacio507' + '\n' + 'Versión del bot: 1.0' + '\n\n' + '__Esta versión está en desarrllo__');
+    + '\n\n' + 'Creador: @horacio507' + '\n' + 'Versión del bot: 1.1' + '\n\n' + 'Estás en la versión beta');
 })
 
 bot.command('info', ctx => {
     ctx.reply('El proposito de este bot es poder ayudarte a organizarte y actualizarte con avisos y novedades.' + '\n\n' + 'Información detallada del bot'
-    + '\n\n' + 'Creador: @horacio507' + '\n' + 'Versión del bot: 1.0' + '\n\n' + '__Esta versión está en desarrllo__');
+    + '\n\n' + 'Creador: @horacio507' + '\n' + 'Versión del bot: 1.1' + '\n\n' + 'Estás en la versión beta');
 })
 
-//Menú con los comandos (manera comandos)
+bot.command('info@HMLA1_BOT', ctx => {
+    ctx.reply('El proposito de este bot es poder ayudarte a organizarte y actualizarte con avisos y novedades.' + '\n\n' + 'Información detallada del bot'
+    + '\n\n' + 'Creador: @horacio507' + '\n' + 'Versión del bot: 1.1' + '\n\n' + 'Estás en la versión beta');
+})
+
+
+//Comandos
 
 bot.action('comandos', ctx => {
     ctx.answerCbQuery(); //para quitar el icono de cargando del botón
-    ctx.reply('Aquí están todos los comandos del bot\n\n' + "Multimedia\n" + "/deberes - Deberes mandados en clase\n" + "/noticias - Ver las últimas 10 noticias de elJuande.com\n\n"
-    + "Sobre el bot\n" + "/info - Quién ha creado este bot\n"
-    +"/license - Ver la licencia de Copyright y consultar el código del bot");
+    ctx.reply("Aquí están algunos de los comandos del bot" + "\n\n" + "Multimedia" + "\n" + "/deberes - Deberes mandados en clase" + "\n" + "/avisos - Avisos, notificaciones..."
+    + "\n" + "/sugerencias - formulario para sugerencias sobre la clase o el bot" +
+    + "\n\n" + "Sobre el bot" + "\n" + "/info - Información sobre el bot");
 })
 
 
